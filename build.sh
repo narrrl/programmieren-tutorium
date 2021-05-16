d42004e1fc5a9735fc2ea00da06fb65cea69ac0a
Nils Pukropp <nils.pukropp@outlook.de>
2021-05-16 05:03:18 +0200

    init

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for folder in tut_*; do
+    cd "$folder"
+    for texFile in *.tex; do
+        echo $texFile
+        xelatex -output-directory="../Build_Output" "$texFile"
+    done
+
+    cd ..
+done
+
+# Clean everything else
+cd Build_Output
+mkdir Save_Me
+cp *.pdf Save_Me
+rm *
+mv Save_Me/* .
+rm -r Save_Me
