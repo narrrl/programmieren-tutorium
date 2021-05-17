f9a4b8b4b0f7890c09dbc9dff927dac1d8b48dc2
Nils Pukropp <nils.pukropp@outlook.de>
2021-05-17 19:46:18 +0200

    moved tut_02 to summer, updated build/clean script

--- build.sh
+++ build.sh
@@ -3,11 +3,13 @@
 mkdir Build_Output
 
 
-for folder in tut_*; do
+for folder in *tut_*; do
     cd "$folder"
     for texFile in *.tex; do
         echo $texFile
         # build
+        xelatex "$texFile"
+        # twice for bookmarks
         xelatex "$texFile"
         find . -maxdepth 1 -type f -name '*.pdf' -exec mv '{}' ../Build_Output/ \;
     done
--- clean.sh
+++ clean.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for folder in tut_*; do
+for folder in *tut_*; do
     cd "$folder"
     # clean files
     find . -maxdepth 1 ! -name '*.tex' -type f -exec rm -f {} +
