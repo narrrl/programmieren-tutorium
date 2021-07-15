ec2c7ffa4b35ca21d000fa781251547bd318094c
Nils Pukropp <nils@nirusu.codes>
2021-07-15 20:50:00 +0200

    added documentation

--- Material/musterloesung/ui/commandenum/Command.java
+++ Material/musterloesung/ui/commandenum/Command.java
@@ -2,18 +2,51 @@ package musterloesung.ui.commandenum;
 
 import musterloesung.entity.Result;
 import musterloesung.game.BasicCalculator;
+import musterloesung.ui.commandpattern.ICommand;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * This class represents the interface between the ui and the logic. <br>
+ * <br>
+ * Where "the logic" is our {@link BasicCalculator} <br>
+ * and "the ui" is our Main function {@link Main#main(String[])} that does all the in/out stuffy <br>
+ *
+ * The main concept behind a shell is ... <br>
+ * ... that the user can input different commands with custom arguments <br>
+ * ... depending on the input a command gets executed and represents the output textual on the console <br>
+ * ... the shell runs in a loop until the user exits the shell
+ *
+ * Each command is an entry in this enum thus being constant and exists globally at compile time.
+ * This is important when we need for example multiple instances. If you're interested in a more common and modern solution
+ * for parsing/executing user input then take a look at the Command-Pattern {@link ICommand}. <br>
+ * <br>
+ *
+ * That said each "command" has an unique identifier which is a String that represents the input pattern of the command.
+ * This string gets compiled by {@link Pattern#compile(String)} and stored as an attribute for each entry {@link Command#pattern}.
+ *
+ * To find the command the user wants to execute we just need to match all Commands to the user input and execute the command that
+ * covers 100% of the user input.
+ *
+ * This happens in {@link Command#executeCommand(String, BasicCalculator)} and is basically a for loop that looks for the right command
+ *
+ * The actual code of each command gets implemented in our abstract/virtual function {@link Command#execute(Matcher, BasicCalculator)}
+ * which has to be implemented by each command.
+ *
+ *
+ * */
 public enum Command {
 
+    // this is our sum command to add two numbers together
     SUM("sum (" + BasicCalculator.NUMBER_REGEX + ") (" + BasicCalculator.NUMBER_REGEX + ")") {
         @Override
         public Result<String> execute(final Matcher matcher, final BasicCalculator calculator) {
             final int augend;
             final int addend;
             try {
+                // a (...) in a regex represents a group which we can access with matcher#group(int), starting with 1
+                // the user doesn't have to write 'sum (1) (6)'
                 augend = Integer.parseInt(matcher.group(1));
                 addend = Integer.parseInt(matcher.group(2));
             } catch (NumberFormatException e) {
@@ -25,6 +58,7 @@ public enum Command {
             return Result.asSuccess(String.valueOf(sum));
         }
     },
+    // this is our exit command to exit the calculator
     EXIT("exit") {
         @Override
         public Result<String> execute(final Matcher matcher, final BasicCalculator calculator) {
@@ -39,20 +73,26 @@ public enum Command {
     private final Pattern pattern;
 
     Command(final String regex) {
+        // here gets the pattern compiled
         pattern = Pattern.compile(regex);
     }
 
+    // this is our abstract execute function that each command overrides
     public abstract Result<String> execute(final Matcher matcher, final BasicCalculator calculator);
 
     public static Result<String> executeCommand(final String input, final BasicCalculator calculator) {
+        // we iterate over every entry in our enum
         for( final Command cmd : Command.values()) {
+            // we match the commands pattern to that of the users input
             final Matcher matcher = cmd.pattern.matcher(input);
 
+            // if we get a total match, we can execute the command and return the result
             if (matcher.matches()) {
                 return cmd.execute(matcher, calculator);
             }
         }
 
+        // if we don't find a matching command we return an error that no command was found
         return Result.asError(COMMAND_NOT_FOUND);
     }
     
--- Material/musterloesung/ui/commandenum/Main.java
+++ Material/musterloesung/ui/commandenum/Main.java
@@ -5,6 +5,14 @@ import musterloesung.game.BasicCalculator;
 
 import java.util.Scanner;
 
+/**
+ * This class is the main entry point for our interactive {@link BasicCalculator}.
+ *
+ * The {@link #main(String[])} takes user input 
+ * which then gets processed by {@link Command#execute(java.util.regex.Matcher, BasicCalculator)}
+ * and depending on the return value {@link Result} we print out the {@link Result#getOutput()} as an error or normally
+ *
+ */
 public final class Main {
 
     public static final String ERROR_PREFIX = "Error, ";
@@ -14,20 +22,29 @@ public final class Main {
     }
 
     public static void main(String[] args) {
+        // we create our basic calculator
+        // this instance of our calculator gets passed down
+        // to our Command-enum
         final BasicCalculator calculator = new BasicCalculator();
+
+        // we create a scanner to get our user input
         final Scanner scanner = new Scanner(System.in);
 
         do {
+            // read user input
             final String input = scanner.nextLine();
 
+            // try to execute user input
             final Result<String> result = Command.executeCommand(input, calculator);
 
+            // take the result and check if it's an error
             if (result.isError()) {
+                // we indicate an error with "Error, ..." in the console
                 System.err.println(ERROR_PREFIX + result.getOutput());
             } else if (!result.getOutput().isEmpty()) {
                 System.out.println(result.getOutput());
             }
-        } while(!calculator.isExited());
+        } while(!calculator.isExited()); // exit the do-while loop if the calculator exited
 
 
 
